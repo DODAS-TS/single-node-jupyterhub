2bc998afc925c707014c7495f9fd7aba708903d7
MircoT <MircoT@users.noreply.github.com>
2021-05-25 11:24:10 +0200

    Fix host ip variable

--- collab_proxy/collab_proxy.py
+++ collab_proxy/collab_proxy.py
@@ -7,12 +7,12 @@ from flask import Flask, redirect, request, Response
 
 from jupyterhub.services.auth import HubAuth
 
-prefix = os.environ.get('JUPYTERHUB_SERVICE_PREFIX', '/')
-jupyter_token = os.environ.get('JUPYTER_TOKEN', '')
-jupyter_token = os.environ.get('HOST_IP', '0.0.0.0')
+prefix = os.environ.get("JUPYTERHUB_SERVICE_PREFIX", "/")
+jupyter_token = os.environ.get("JUPYTER_TOKEN", "")
+host_ip = os.environ.get("HOST_IP", "0.0.0.0")
 
 auth = HubAuth(
-    api_token=os.environ['JUPYTERHUB_API_TOKEN'],
+    api_token=os.environ["JUPYTERHUB_API_TOKEN"],
     cache_max_age=60,
 )
 
@@ -21,6 +21,7 @@ app = Flask(__name__)
 
 def authenticated(f):
     """Decorator for authenticating with the Hub"""
+
     @wraps(f)
     def decorated(*args, **kwargs):
         cookie = request.cookies.get(auth.cookie_name)
@@ -36,19 +37,20 @@ def authenticated(f):
         else:
             # redirect to login url on failed auth
             # return redirect(auth.login_url + '?next=%s' % quote(request.path))
-            return redirect(f"https://{HOST_IP}:8888/login?")
+            return redirect(f"https://{host_ip}:8888/login?")
+
     return decorated
 
 
 @app.route(prefix)
 @authenticated
 def go_to_jupyterlab_collab(user):
-    return redirect(f"https://{HOST_IP}:8889/lab?token={jupyter_token}")
+    return redirect(f"https://{host_ip}:8889/lab?token={jupyter_token}")
     # return Response(
     #     json.dumps(user, indent=1, sort_keys=True),
     #     mimetype='application/json',
     #     )
-    
-if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=8099)
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8099)
